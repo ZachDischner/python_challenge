a94da7a29a7b43da9aa4023530efcc21833e276e
Zach Dischner <zach.dischner@colorado.edu>
2017-03-30 13:11:14 -0600

    Updated regex to filter out invalid addresses

--- ipparser.py
+++ ipparser.py
@@ -33,7 +33,6 @@ TODO/Improvements:
 # ----------*----------*----------*----------*----------*----------*----------*
 import os
 import sys
-import logging
 import re
 import utils
 
@@ -42,23 +41,38 @@ _here = os.path.dirname(os.path.realpath(__file__))
 logger = utils.logger
 
 # Regex pattern for IP address constraining each section to 0-255
-_IP_PATTERN = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+_IP_PATTERN = "\\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b"
 _IP_FILE = os.path.join(_here, "list_of_ips.txt")
 
 ##############################################################################
 #                                   Functions
 # ----------*----------*----------*----------*----------*----------*----------*
+def ipsearch(searchstring, pattern=_IP_PATTERN):
+    """Search a string for an IP address
+
+    Examples:
+        >>> ipsearch("foo")
+        []
+        >>> ipsearch("Praesent in tincidunt 33.33.53.155. In in vehicula 233.151.2.99")
+        ['33.33.53.155', '233.151.2.99']
+        >>> ipsearch("Okay: 33.33.53.155 . Bad 211.999.191.99")
+        ['33.33.53.155']
+    """
+    matches = [match.group() for match in re.finditer(pattern, searchstring)]
+    if matches:
+        logger.debug(f"Found {len(matches)} IP addresses in string '{searchstring}")
+    return matches
+
 
 
 def extract_ips(fname, pattern=_IP_PATTERN, limit=10):
     extracted = 0
     with open(fname, 'r') as _file:
         for line in _file.readlines():
-            matches = re.finditer(pattern,line)
+            matches = ipsearch(line, pattern=pattern)
             for match in matches:
                 extracted += 1
-                logger.debug(f"Found IP match(es) in {line}")
-                yield match.group()
+                yield match
                 if extracted > limit:
                     logger.debug(f"Maximum parsing limit of {limit} reached.\nStopping parsing of {fname} for IP addresses early")
                     return

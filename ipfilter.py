e1b9fb166543805eecdf13070833c493f7d04766
Zach Dischner <zach.dischner@colorado.edu>
2017-03-31 10:51:09 -0600

    Updated Filtering class, modularized filter/search functions for GEO/RDAP and reworked filter results to return another IPFilter instance

--- ipfilter.py
+++ ipfilter.py
@@ -71,6 +71,10 @@ class IPFilterer(object):
     """
     Queries will be ran against GEO information first, then RDAP info second.
     """
+    @property
+    def content(self):
+        return self.to_json()
+
     def __init__(self, data=None, df_geo=None, df_rdap=None):
         """Class to help search/filter out GEO and RDAP IP address information
         
@@ -79,21 +83,71 @@ class IPFilterer(object):
         """
         if data:
             df_geo, df_rdap = process_datastore(data)
-
+        
+        ## Internally stored DataFrames of ip address metadata
         self.df_geo = df_geo
         self.df_rdap = df_rdap
 
+        ## Handy attribute, store searchable keys
+        self.searchable = sorted(unique(list(self.df_rdap.keys()) + list(self.df_geo.keys())))
+    
+    def __repr__(self):
+        return f"Filterable IP dataset with {len(self.df_geo)} addresses in it"
+    
+    def _filter_geo(self,key,value):
+        """Base function to get ip addresses that match `key`==`value` condition for GEO metadata
+
+        Returns:
+            _:  (list) - List of IP addresses that match that condition 
+        """
+        return self.df_geo.query(f"""{key}=={value}""")['ip'].values
+    
+    def ip_subset(self, ips):
+        """Take a subset of GEO/RDAP metadata information just for ip addresses `ips`
+
+        As long as at least one address in `ips` exists in the dataframes, subsets will be returned.
+        """
+        return self.df_geo.loc[ips], self.df_rdap.loc[ips]
+
+    def _filter_rdap(self,key,value):
+        return self.df_rdap.query(f"""{key}=={value}""")['ip'].values
+
     def filter(self,key,value):
+        """Filter IP metadata
+
+        Filters according to where `key`==`value`. This method searches through GEO information
+        first for a match, then through RDAP information. In either case, whererver the filtering
+        conditions are met, the GEO and RDAP information is returned for that subset of IP address.
+
+        Explicitly, filtering is NOT done in place. 
+
+        Returns:
+            _:      (IPFilterer()) - A new IPFilterer() object containing just the filtered data subset
+        """
+        ## First check that the search should even be performed
+        if key not in self.searchable:
+            logger.warn(f"IP GEO or RDAP metadata store has no attribute '{key}'. Empty store returned")
+            logger.debug(f"Attributes you can search through: {self.searchable}")
+            return IPFilterer(df_rdap=pd.DataFrame(), df_geo=pd.DataFrame())
+        
+        ## Condition arguments
         if isinstance(value, str):
             value = f"'{value}'"
 
-        results_geo = self.df_geo.query(f"""{key}=={value}""")
-        if len(results_geo) > 0:
-            results_rdap = self.df_rdap.loc[results_geo.index]
+        ## Try to filter GEO dataset
+        if key in self.df_geo.keys():
+            matching_ips = self._filter_geo(key,value)
         else:
-            results_rdap = self.df_rdap.query(f"""{key}=={value}""")
-            results_geo = self.df_geo.loc[results_rdap.index]
-        return results_geo, results_rdap
+            matching_ips = self._filter_rdap(key,value)
+        
+        df_geo_subset, df_rdap_subset = self.ip_subset(matching_ips)
+
+        return IPFilterer(df_geo=df_geo_subset, df_rdap=df_rdap_subset)
+    
+    def to_dict(self):
+        """Convert (potentially) filtererd IP metadata back into a dictionary that matches JSON data stores
+        """
+        return {ip:{"RDAP":self.df_rdap.loc[ip].to_dict(), "GEO":self.df_geo.loc[ip].to_dict()} for ip in self.df_geo['ip']}
 
 
 
